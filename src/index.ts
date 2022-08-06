143a28144ec9d0767e0ae98cfb6f67d25fda60f4
Alejandro Sebastian Herrero <110503112+Aleherre14@users.noreply.github.com>
2022-08-06 11:15:58 -0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,23 @@
+import "./styles.css";
+
+document.getElementById("app").innerHTML = `
+<h1>Hello Vanilla!</h1>
+<div>
+  We use the same configuration as Parcel to bundle this sandbox, you can find more
+  info about Parcel 
+  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
+</div>`;
+
+console.log("-Compras Supermercado");
+console.log("paso 1: armar lista de compras");
+console.log("paso 2: agarrar bolsa ecologica y el auto para ir");
+console.log("paso 3: hacer 5 cuadras por av san martin");
+console.log("paso 4: estacionar el auto y bajarse al supermercado");
+console.log("paso 5: realizar las compras de la lista");
+console.log("paso 6: hacer la cola para pagar");
+console.log("paso 7: abonar y guardar la mercaderia en la bolsa");
+console.log(
+  "paso 8: volver al auto y dirigirse por san martin 5 cuadras hasta casa"
+);
+console.log("paso 9: una vez que llegamos bajar en casa");
+console.log("paso 10: acomodar la mercaderia");
